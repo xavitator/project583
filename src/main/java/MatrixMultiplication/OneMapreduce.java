d22682c549bca7cba63da0e00f78ebd287989ab0
MeryJa <meryemjaaidan@gmail.com>
2022-03-20 22:48:11 +0100

    End of part B

--- src/main/java/MatrixMultiplication/OneMapreduce.java
+++ src/main/java/MatrixMultiplication/OneMapreduce.java
@@ -1,48 +1,127 @@
 package MatrixMultiplication;
 
-import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.*;
+import java.io.IOException;
+import java.util.HashMap;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+public class OneMapreduce {
+	public static class MatrixMapperVector extends Mapper<Object, Text, Text, Text>
 
-import scala.Tuple2;
+	{
 
-public class OneMapreduce
-{
-    public static void main( String[] args )
-    {
-    	String inputFile = "edgelist.txt";
-    	String outputFolder = "output";
+		public void map(Object key, Text value, Context context) throws IOException, InterruptedException
 
-    	// Create a Java Spark Context
-    	SparkConf conf = new SparkConf().setMaster("local[*]").setAppName("Spark");
-    	JavaSparkContext sc = new JavaSparkContext(conf);
+		{
 
-		// Create Vector map
-		JavaRDD<String> input_idlabels = sc.textFile("idslabels.txt");
-		Map<Integer, Double> vector = input_idlabels.mapToPair( s -> new Tuple2<Integer,Double>( Integer.parseInt(s.split(" ")[0]), 1.0/64375)).collectAsMap();
+			Configuration conf = context.getConfiguration();
+			int m = Integer.parseInt(conf.get("m"));
+			int p = Integer.parseInt(conf.get("p"));
+			String line = value.toString();
+			String[] indicesAndValue = line.split("\t");
+			Text outputKey = new Text();
+			Text outputValue = new Text();
+			for (int i = 0; i < m; i++)
+			{
+				outputKey.set(i + "," + "0");
+				outputValue.set("N," + indicesAndValue[0] + "," + indicesAndValue[1]);
+				context.write(outputKey, outputValue);
+			}
+		}
+	}
 
-    	JavaRDD<String> input = sc.textFile(inputFile);
+	public static class MatrixMapperMatrix extends Mapper<Object, Text, Text, Text>
+	{
+		public void map(Object key, Text value, Context context) throws IOException, InterruptedException
+		{
+			Configuration conf = context.getConfiguration();
+			int m = Integer.parseInt(conf.get("m"));
+			int p = Integer.parseInt(conf.get("p"));
+			String line = value.toString();
+			String[] indicesAndValue = line.split(" ");
+			Text outputKey = new Text();
+			Text outputValue = new Text();
+			for (int i = 1; i < indicesAndValue.length; i++) {
+				for (int k = 0; k < p; k++) {
+					outputKey.set(indicesAndValue[0] + "," + k);
+					outputValue.set("M," + indicesAndValue[i] + "," + "1");
+					context.write(outputKey, outputValue);
+				}
+			}
+		}
+	}
+	
+	public static class MatrixReducer extends Reducer<Text, Text, Text, Text>
+	{
+		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException
+		{
+			String[] value;
+			HashMap<Integer, Float> hashA = new HashMap<Integer, Float>();
+			HashMap<Integer, Float> hashB = new HashMap<Integer, Float>();
+			for (Text val : values)
+			{
+				value = val.toString().split(",");
+				if (value[0].equals("M"))
+				{
+					hashA.put(Integer.parseInt(value[1]), Float.parseFloat(value[2]));
+				}
+				else
+				{
+					hashB.put(Integer.parseInt(value[1]), Float.parseFloat(value[2]));
+				}
+			}
+			int n = Integer.parseInt(context.getConfiguration().get("n"));
+			float result = 0.0f;
+			float a_ij;
+			float b_jk;
+			for (int j = 0; j < n; j++)
+			{
+				a_ij = hashA.containsKey(j) ? hashA.get(j) : 0.0f;
+				b_jk = hashB.containsKey(j) ? hashB.get(j) : 0.0f;
+				result += a_ij * b_jk;
+			}
+			if (result != 0.0f)
+			{
+				context.write(new Text(""), new Text(key.toString() + "\t" + Float.toString(result)));
+			}
+		}
+	}
 
-		// MAP
-		JavaPairRDD<Integer,List<String>> mapped_matrix_1 = input.mapToPair(s -> {
-			List<String> line = new ArrayList<String>(Arrays.asList(s.split(" ")));
-			Integer entry =  Integer.parseInt(line.get(0));
-			line.remove(0);
-			return new Tuple2<Integer, List<String>>(entry, line);
-		});
-		
-		JavaPairRDD<Integer, String> mapped_matrix_2 = mapped_matrix_1.flatMapValues(s->
-		s.iterator());
-		JavaPairRDD<Integer,Double> mapped_matrix = mapped_matrix_2.mapValues(Integer::parseInt).mapValues(vector::get);
-		
-		//REDUCE
-		JavaPairRDD<Integer,Double> reduce_output = mapped_matrix.reduceByKey((a,b) -> a+b);
+	static void matrixMultiplicationUsingOneMapReduce(String input, String inputR, String outputFolder)
+			throws IOException, ClassNotFoundException, InterruptedException {
+		Configuration conf = new Configuration();
+		// M is an m-by-n matrix; N is an n-by-p matrix.
+		conf.set("m", "64375");
+		conf.set("n", "64375");
+		conf.set("p", "1");
+		Job job1 = Job.getInstance(conf, "MatrixMultiplication");
+		job1.setJarByClass(PartB_3_Using_One_MapReduce.class);
+		job1.setReducerClass(MatrixReducer.class);
+		job1.setOutputKeyClass(Text.class);
+		job1.setOutputValueClass(Text.class);
 
-		reduce_output.foreach(data -> System.out.println(data._1 + " " + data ._2));
-    }
+		job1.setInputFormatClass(TextInputFormat.class);
+		job1.setOutputFormatClass(TextOutputFormat.class);
 
+		MultipleInputs.addInputPath(job1, new Path("graph/edgelist.txt"), TextInputFormat.class, MatrixMapperVector.class);
+		MultipleInputs.addInputPath(job1, new Path("graph/vector.txt"), TextInputFormat.class, MatrixMapperMatrix.class);
+
+		FileSystem fs = FileSystem.get(conf);
+		if (fs.exists(new Path(outputFolder)))
+			fs.delete(new Path(outputFolder), true);
+
+		TextOutputFormat.setOutputPath(job1, new Path(outputFolder));
+		job1.waitForCompletion(true);
+	}
+public static void main(String args[]) throws ClassNotFoundException, IOException, InterruptedException {
+	matrixMultiplicationUsingOneMapReduce("graph","inputR","output1");
+}
 }
--- src/main/java/MatrixMultiplication/TwoMapreduce.java
+++ src/main/java/MatrixMultiplication/TwoMapreduce.java
@@ -1,52 +1,194 @@
 package MatrixMultiplication;
 
-import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaPairRDD;
-import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.JavaSparkContext;
-import scala.Tuple2;
-
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.io.IOException;
 
-public class TwoMapreduce
-{
-    public static void main( String[] args )
-    {
-    	//Initiate the input file and output folder
-    	String inputFile = "edgelist.txt";
-    	String outputFolder = "output";
+import java.util.StringTokenizer;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.*;
+import org.apache.spark.api.java.function.*;
 
-    	// Create a Java Spark Context
-    	SparkConf conf = new SparkConf().setMaster("local[*]").setAppName("Spark");
-    	JavaSparkContext sc = new JavaSparkContext(conf);
 
-		// Create Vector map
-		JavaRDD<String> input_idlabels = sc.textFile("idslabels.txt");
-		Map<Integer, Double> vector = input_idlabels.mapToPair( s -> new Tuple2<Integer,Double>( Integer.parseInt(s.split(" ")[0]), 1.0/64375)).collectAsMap();
-		
-    	JavaRDD<String> input = sc.textFile(inputFile);
+import scala.Tuple2;
 
-		// MAP
-		JavaPairRDD<Integer,List<String>> mapped_matrix_1 = input.mapToPair(s -> {
-			List<String> line = new ArrayList<String>(Arrays.asList(s.split(" ")));
-			Integer entry =  Integer.parseInt(line.get(0));
-			line.remove(0);
-			return new Tuple2<Integer, List<String>>(entry, line);
-		});
-		// Flatten
-		JavaPairRDD<Integer,String> mapped_matrix_2 = mapped_matrix_1.flatMapValues(value ->
-		value.iterator());
-		
-		//Parse the strings to double
-		JavaPairRDD<Integer, Double> mapped_matrix = mapped_matrix_2.mapValues(Integer::parseInt).mapValues(vector::get);
-				
-		//REDUCE
-		JavaPairRDD<Integer,Double> reduce_output = mapped_matrix.reduceByKey((a,b) -> a+b);
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.ArrayWritable;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.mapred.InputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.mapred.lib.IdentityMapper;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Reducer.Context;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.join.TupleWritable;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-		reduce_output.foreach(data -> System.out.println(data._1 + " " + data ._2));
-    }
+public class TwoMapreduce{
+
+	public static class MatrixMapperVector2_1 extends Mapper<Object, Text, Text, Text>
+
+	{
+
+		public void map(Object key, Text value, Context context) throws IOException, InterruptedException
+
+		{
+			Configuration conf = context.getConfiguration();
+			String line = value.toString();
+			Text outputKey = new Text();
+			Text outputValue = new Text();
+			outputKey.set(line.split("\t")[0]);
+			outputValue.set("N," + "0" + "," + line.split("\t")[1]);
+			context.write(outputKey, outputValue);
+		}
+	}
+
+	public static class MatrixMapperMatrix2_1 extends Mapper<Object, Text, Text, Text>
+	{
+		public void map(Object key, Text value, Context context) throws IOException, InterruptedException
+		{
+			Configuration conf = context.getConfiguration();
+			String[] nodes = value.toString().split(" ");
+			Text outputKey = new Text();
+			Text outputValue = new Text();
+			for (int i = 1; i < nodes.length; i++) {
+				outputKey.set(nodes[i]);
+				outputValue.set("M," + nodes[0] + "," + "1");
+				context.write(outputKey, outputValue);
+			}
+		}
+	}
+
+	public static class MatrixReducer2_1 extends Reducer<Text, Text, Text, Text>
+	{
+		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException
+		{
+			String[] value;
+			HashMap<Integer, Float> hashA = new HashMap<Integer, Float>();
+			HashMap<Integer, Float> hashB = new HashMap<Integer, Float>();
+			for (Text val : values)
+			{
+				value = val.toString().split(",");
+				if (value[0].equals("M"))
+				{
+					hashA.put(Integer.parseInt(value[1]), Float.parseFloat(value[2]));
+				}
+				else
+				{
+					hashB.put(Integer.parseInt(value[1]), Float.parseFloat(value[2]));
+				}
+			}
+			float a_ij;
+			float b_jk;
+
+			for (Entry<Integer, Float> b : hashB.entrySet()) {
+				float result = 0;
+				context.write(new Text(key.toString() + "," + Integer.toString(b.getKey())),
+						new Text(Float.toString(result)));
+			}
+
+			for (Entry<Integer, Float> a : hashA.entrySet()) {
+				for (Entry<Integer, Float> b : hashB.entrySet()) {
+					a_ij = a.getValue();
+					b_jk = b.getValue();
+					float result = a_ij * b_jk;
+					context.write(new Text(Integer.toString(a.getKey()) + "," + Integer.toString(b.getKey())),
+							new Text(Float.toString(result)));
+				}
+			}
+		}
+	}
+
+	public static class MatrixMapper2_2 extends Mapper<Object, Text, Text, Text>
+	{
+		public void map(Object key, Text value, Context context) throws IOException, InterruptedException
+		{
+			Configuration conf = context.getConfiguration();
+			context.write(new Text(value.toString().split("\t")[0]), new Text(value.toString().split("\t")[1]));
+		}
+	}
+
+	public static class MatrixReducer2_2 extends Reducer<Text, Text, Text, Text>
+	{
+		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException
+		{
+			String value;
+			float result = (float) 0.0;
+			for (Text val : values)
+			{
+				value = val.toString();
+				result += Float.parseFloat(value);
+			}
+			context.write(new Text(key.toString().split(",")[0]), new Text(Float.toString(result)));
+		}
+	}
+
+	static void matrixMultiplicationUsingTwoMapReduce(String input, String inputR, String outputFolder)
+			throws IOException, ClassNotFoundException, InterruptedException {
+		Configuration conf = new Configuration();
+		// M is an m-by-n matrix; N is an n-by-p matrix.
+		conf.set("m", "64375");
+		conf.set("n", "64375");
+		conf.set("p", "1");
+
+		Job job1 = Job.getInstance(conf, "MatrixMultiplication");
+		job1.setJarByClass(TwoMapreduce.class);
+		job1.setReducerClass(MatrixReducer2_1.class);
+		job1.setOutputKeyClass(Text.class);
+		job1.setOutputValueClass(Text.class);
+
+		job1.setInputFormatClass(TextInputFormat.class);
+		job1.setOutputFormatClass(TextOutputFormat.class);
+
+		MultipleInputs.addInputPath(job1, new Path(inputR), TextInputFormat.class, MatrixMapperVector2_1.class);
+
+		MultipleInputs.addInputPath(job1, new Path(input), TextInputFormat.class, MatrixMapperMatrix2_1.class);
+
+		FileSystem fs = FileSystem.get(conf);
+		if (fs.exists(new Path("outputIntermediate")))
+			fs.delete(new Path("outputIntermediate"), true);
+
+		TextOutputFormat.setOutputPath(job1, new Path("outputIntermediate"));
+		job1.waitForCompletion(true);
+
+		conf = new Configuration();
+
+		job1 = Job.getInstance(conf, "MatrixMultiplication-part2");
+		job1.setJarByClass(TwoMapreduce.class);
+		job1.setMapperClass(MatrixMapper2_2.class);
+		job1.setCombinerClass(MatrixReducer2_2.class);
+		job1.setReducerClass(MatrixReducer2_2.class);
+		job1.setOutputKeyClass(Text.class);
+		job1.setOutputValueClass(Text.class);
+		FileInputFormat.addInputPath(job1, new Path("outputIntermediate"));
+		fs = FileSystem.get(conf);
+		if (fs.exists(new Path(outputFolder)))
+			fs.delete(new Path(outputFolder), true);
+		FileOutputFormat.setOutputPath(job1, new Path(outputFolder));
+		job1.waitForCompletion(true);
+
+	}
+	
+	public static void main(String args[]) throws ClassNotFoundException, IOException, InterruptedException {
+		matrixMultiplicationUsingTwoMapReduce("graph","graph","output");
+	}
 
 }
